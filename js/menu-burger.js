a9fe071ea4c6581aa0235223c2126fac14193487
Denis Korotaevsky <huligan8771@gmail.com>
2022-04-11 13:35:04 +0300

    commit

---
+++ js/menu-burger.js
@@ -0,0 +1,15 @@
+let header__burger = document.querySelector('.header_burger');
+let header_menu = document.querySelector('.header_menu');
+let back = document.querySelector('body');
+let header__list = document.querySelector('.header_box_item');
+
+header__burger.onclick = function(){
+    header__burger.classList.toggle('active');
+    header_menu.classList.toggle('active');
+    back.classList.toggle('lock');
+}
+
+header__list.onclick = function () {
+    header__list.classList.remove('active');
+    back.classList.toggle('lock');
+}
---
+++ js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $(".slider").slick({
+        dots: true,
+        slidesToShow: 5,
+        slidesToScroll: 2,
+        Infinity: true,
+        speed: 500,
+        initialSlade: 2,
+        responsive:[
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
+});
